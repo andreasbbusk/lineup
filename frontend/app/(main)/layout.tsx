a233f0f055258a99852ed862f3eba3839dd67d8e
andreaskadhede <andreaskadhede@icloud.com>
2025-12-08 11:14:35 +0100

    refactor: clean up layout component structure and formatting

--- frontend/app/(main)/layout.tsx
+++ frontend/app/(main)/layout.tsx
@@ -1,15 +1,22 @@
 import type { ReactNode } from "react";
+
 import BottomNav from "@/app/components/bottom-nav";
 
 interface MainLayoutProps {
-  children: ReactNode;
+	children: ReactNode;
 }
 
 export default function Layout({ children }: MainLayoutProps) {
-  return (
-    <div className="flex flex-col min-h-screen">
-      <div className="mx-auto max-w-5xl px-6 py-8">{children}</div>
-      <BottomNav />
-    </div>
-  );
+	return (
+		<div
+		// className="flex flex-col min-h-screen"
+		>
+			<div
+			// className="mx-auto max-w-5xl "
+			>
+				{children}
+			</div>
+			<BottomNav />
+		</div>
+	);
 }
