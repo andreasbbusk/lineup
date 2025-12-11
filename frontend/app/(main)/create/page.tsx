fb2a0bd6c195ffbca1e8f40384aa3a939b998284
andreaskadhede <andreaskadhede@icloud.com>
2025-12-11 15:47:59 +0100

    fix: add styling to main container for consistent layout

--- frontend/app/(main)/create/page.tsx
+++ frontend/app/(main)/create/page.tsx
@@ -4,7 +4,7 @@ import { CreateForm } from "@/app/modules/features/posts/components/create-form"
 
 export default function Page() {
 	return (
-		<main>
+		<main className="bg-white h-[92vh]">
 			<CreateForm />
 		</main>
 	);
